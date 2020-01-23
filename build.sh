956c6b20a086044011eb14698ec37b204f3acf16
Miguel Caro <mcaroba@gmail.com>
2020-01-23 15:56:14 +0200

    Fix build file

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 #debug="-g -fcheck=all"
+mkdir -p bin/
 gfortran -c printouts.f90 crystal_special.f90
-gfortran -o gen_kpts gen_kpts.f90 *.o
-gfortran $debug -o get_bands get_bands.f90
+gfortran -o bin/gen_kpts gen_kpts.f90 *.o
+gfortran $debug -o bin/get_bands get_bands.f90
